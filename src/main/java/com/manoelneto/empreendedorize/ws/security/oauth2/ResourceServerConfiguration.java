1c7c154b1442fc0d37ce442fddba1c416ffcf25b
Manoel <manoelfernandes15@gmail.com>
2019-06-25 18:39:21 -0300

    Meu backend

--- src/main/java/com/manoelneto/empreendedorize/ws/security/oauth2/ResourceServerConfiguration.java
+++ src/main/java/com/manoelneto/empreendedorize/ws/security/oauth2/ResourceServerConfiguration.java
@@ -35,5 +35,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .exceptionHandling()
                 .accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
+    
 }
 
